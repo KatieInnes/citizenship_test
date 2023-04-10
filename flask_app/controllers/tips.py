cde1da42e160b881446c2691808aa37fa18ef84d
KatieInnes <katharine.innes@gmail.com>
2023-04-10 13:10:10 -0700

    created Q&A MVC

--- flask_app/controllers/tips.py
+++ flask_app/controllers/tips.py
@@ -4,12 +4,27 @@ from flask_app.models.tip import Tip
 from flask_app.models.user import User
 
 @app.route('/advicecenter/edit', methods = ["POST"])
-def edit():
+def edit_tip():
     Tip.edit(request.form)
     return redirect ('/advicecenter')
 
 
+# If the user is not in session, they cannot delete the tip.
+
 @app.route('/advicecenter/delete/<int:tip_id>')
-def delete(tip_id):
-    Tip.delete(tip_id)
+def delete_tip(id):
+
+    if "id" not in session:
+        return redirect('/logout')
+
+    data = {
+        "id": id
+    }
+
+    tip = Tip.view_tip(id)
+
+    if tip and tip.user == int(session["id"]):
+        Tip.delete_tip(data)
+
     return redirect('/advicecenter')
+

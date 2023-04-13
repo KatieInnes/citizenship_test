f8461298136efc37beb1983e120587c56185fede
KatieInnes <katharine.innes@gmail.com>
2023-04-13 13:34:58 -0700

    attempts to make the question functionality work

--- flask_app/controllers/questions_answers.py
+++ flask_app/controllers/questions_answers.py
@@ -12,32 +12,32 @@ from flask_app.models.question_answer import QuestionAnswer
 
 
 
-# @app.route('/question/<int:page_id>')
-# def question():
-#     if "id" not in session:
-#         return redirect('/logout/')
+@app.route('/question/<int:page_id>')
+def question():
+    if "id" not in session:
+        return redirect('/logout/')
 
-#     if "page_id" is 1:
-#         session['answers'] = [];
-#         session['questions'] = QuestionAnswer.get_ten_questions();
+    if "page_id" is 1:
+        session['answers'] = [];
+        session['questions'] = QuestionAnswer.get_ten_questions();
 
-#     my_questions = session['question']
-#     return render_template("question.html", question_number_page=page_id, questions = my_questions[id])
+    my_questions = session['questions']
+    return render_template("question.html", question_number_page=page_id, questions = my_questions[id])
 
 
 
-# @app.route('/question/<int:page_id>/submit')
-# def view_sighting(id):
+@app.route('/question/<int:page_id>', methods=["POST"])
+def answer_question(id):
 
-#     answer_array = session['answers'];
-#     answer_array.push(answer_form)
-#     session['answers'] = answer_array;
+    answer_array = session['answers'];
+    answer_array.push(answer_form);
+    session['answers'] = answer_array;
     
 
-#     if "page_id" is 10
-#         return redirect('/results')
-#     else
-#         return redirect('/question/{page_id+1}')
+    # if "page_id" == 10;
+    #     return redirect('/results')
+    # else
+    return redirect('/question/{page_id+1}')
 
 # @app.route('/answers')
 # def view_sighting(id):
